06dd4565c05cbf255ceeea61f462ab8b3eda5f32
banri <banri.yanahama@gmail.com>
2024-09-18 18:54:36 +0900

    update creating trade object and naming #6

--- src/services/fill-service.ts
+++ src/services/fill-service.ts
@@ -6,13 +6,11 @@ import { MockERC20 as ERC20 } from '@banr1/uniswapx-sdk/dist/src/contracts';
 import { logger } from '../logger';
 import { getSupportedToken } from '../utils';
 import { BigNumber, ContractReceipt, ethers, utils, Wallet } from 'ethers';
-import { computePoolAddress, FeeAmount, Pool, Route, SwapQuoter, SwapRouter, Trade } from '@uniswap/v3-sdk';
-import { CurrencyAmount, Percent, Token, TradeType } from '@uniswap/sdk-core';
+import { computePoolAddress, FeeAmount, Pool, Route, SwapRouter, Trade } from '@uniswap/v3-sdk';
+import { CurrencyAmount, Percent, Token } from '@uniswap/sdk-core';
 import { config } from '../config';
-import JSBI from 'jsbi';
-import { POOL_FACTORY_ADDRESS, QUOTER_CONTRACT_ADDRESS, SWAP_ROUTER_ADDRESS } from '../constants';
+import { POOL_FACTORY_ADDRESS, SWAP_ROUTER_ADDRESS } from '../constants';
 import { UniswapV3Pool__factory } from '../types/typechain';
-import { Result } from 'ethers/lib/utils';
 
 interface FillServiceConstructorArgs {
   wallet: Wallet;
@@ -36,6 +34,7 @@ export class FillService {
     this.outputTokens = outputTokens;
   }
 
+  // Fill the intent and swap the input token back to the original token
   async fillIntent({ intent, signature }: { intent: CosignedV2DutchOrder; signature: string }): Promise<void> {
     let txReceipt: ContractReceipt;
     try {
@@ -45,13 +44,14 @@ export class FillService {
       throw error;
     }
     try {
-      await this.takeBackOutputToken(intent, txReceipt);
+      await this.swapInputTokenBackToOriginalToken(intent, txReceipt);
     } catch (error) {
-      logger.error(`Failed to take back the output token 🚨: ${error}`);
+      logger.error(`Failed to swap the input token back to the original token 🚨: ${error}`);
       throw error;
     }
   }
 
+  // Execute the fill intent transaction
   private async executeFill(intent: CosignedV2DutchOrder, signature: string): Promise<ContractReceipt> {
     const signedIntent = {
       order: intent.serialize(),
@@ -66,7 +66,11 @@ export class FillService {
     return receipt;
   }
 
-  private async takeBackOutputToken(intent: CosignedV2DutchOrder, txReceipt: ContractReceipt): Promise<void> {
+  // Swap the input token back to the original token to prepare for the next fill
+  private async swapInputTokenBackToOriginalToken(
+    intent: CosignedV2DutchOrder,
+    txReceipt: ContractReceipt,
+  ): Promise<void> {
     const inputTokenTransferEvent = txReceipt.events!.find(
       event =>
         event.event === 'Transfer' &&
@@ -77,52 +81,56 @@ export class FillService {
       logger.error('Failed to find the transfer event for the input token 🚨');
       return;
     }
-    // get the data value from the event
     const receivedInputTokenAmount = Number(inputTokenTransferEvent.data);
 
-    const inputToken = getSupportedToken(intent.info.input, this.inputTokens);
-    if (inputToken === null) {
+    const inputTokenErc20 = getSupportedToken(intent.info.input, this.inputTokens);
+    if (inputTokenErc20 === null) {
       logger.error('Failed to find the input token 🚨');
       return;
     }
-    const outputToken = getSupportedToken(intent.info.outputs[0]!, this.outputTokens);
-    if (outputToken === null) {
+    const outputTokenErc20 = getSupportedToken(intent.info.outputs[0]!, this.outputTokens);
+    if (outputTokenErc20 === null) {
       logger.error('Failed to find the output token 🚨');
       return;
     }
 
-    const tokenA = new Token(
+    const inputToken = new Token(
       config.chainId,
-      inputToken.address,
-      await inputToken.decimals(),
-      await inputToken.symbol(),
-      await inputToken.name(),
+      inputTokenErc20.address,
+      await inputTokenErc20.decimals(),
+      await inputTokenErc20.symbol(),
+      await inputTokenErc20.name(),
     );
-    const tokenB = new Token(
+    const originalToken = new Token(
       config.chainId,
-      outputToken.address,
-      await outputToken.decimals(),
-      await outputToken.symbol(),
-      await outputToken.name(),
+      outputTokenErc20.address,
+      await outputTokenErc20.decimals(),
+      await outputTokenErc20.symbol(),
+      await outputTokenErc20.name(),
     );
     const poolAddress = computePoolAddress({
       factoryAddress: POOL_FACTORY_ADDRESS,
-      tokenA,
-      tokenB,
+      tokenA: inputToken,
+      tokenB: originalToken,
       fee: FeeAmount.MEDIUM,
     });
     const poolContract = UniswapV3Pool__factory.connect(poolAddress, this.provider);
     const [slot0, liquidity] = await Promise.all([poolContract.slot0(), poolContract.liquidity()]);
-    const pool = new Pool(tokenA, tokenB, FeeAmount.MEDIUM, liquidity.toString(), slot0[0].toString(), slot0[1]);
-    const swapRoute = new Route([pool], tokenA, tokenB);
-    const amountOut = await this.getOutputQuote(swapRoute, tokenA, receivedInputTokenAmount);
-    const uncheckedTrade = Trade.createUncheckedTrade({
-      route: swapRoute,
-      inputAmount: CurrencyAmount.fromRawAmount(tokenA, utils.formatUnits(receivedInputTokenAmount, tokenA.decimals)),
-      outputAmount: CurrencyAmount.fromRawAmount(tokenB, JSBI.BigInt(amountOut)),
-      tradeType: TradeType.EXACT_INPUT,
-    });
-    const methodParameters = SwapRouter.swapCallParameters(uncheckedTrade, {
+    const pool = new Pool(
+      inputToken,
+      originalToken,
+      FeeAmount.MEDIUM,
+      liquidity.toString(),
+      slot0[0].toString(),
+      slot0[1],
+    );
+    const swapRoute = new Route([pool], inputToken, originalToken);
+    // const amountOut = await this.getOutputQuote(swapRoute, inputToken, receivedInputTokenAmount);
+    const trade = await Trade.exactIn(
+      swapRoute,
+      CurrencyAmount.fromRawAmount(inputToken, utils.formatUnits(receivedInputTokenAmount, inputToken.decimals)),
+    );
+    const methodParameters = SwapRouter.swapCallParameters(trade, {
       slippageTolerance: new Percent(50, 10000),
       deadline: Math.floor(Date.now() / 1000) + 60 * 20,
       recipient: this.wallet.address,
@@ -132,28 +140,30 @@ export class FillService {
       to: SWAP_ROUTER_ADDRESS,
       from: this.wallet.address,
       value: BigNumber.from(methodParameters.value),
-      maxFeePerGas: 100000000000,
-      maxPriorityFeePerGas: 1000000000,
+      maxFeePerGas: 100_000_000_000,
+      maxPriorityFeePerGas: 100_000_000_000,
     };
     const tx = await this.wallet.sendTransaction(txToSend);
-    await tx.wait();
+    const receipt = await tx.wait();
+    logger.info('Swapped the input token back to the original token successfully 🎉');
+    logger.info(`receipt: ${receipt}`);
   }
 
-  async getOutputQuote(route: Route<Token, Token>, tokenA: Token, amountIn: number): Promise<Result> {
-    const { calldata } = await SwapQuoter.quoteCallParameters(
-      route,
-      CurrencyAmount.fromRawAmount(tokenA, utils.formatUnits(amountIn, tokenA.decimals).toString()),
-      TradeType.EXACT_INPUT,
-      {
-        useQuoterV2: true,
-      },
-    );
+  // private async getOutputQuote(route: Route<Token, Token>, tokenIn: Token, amountIn: number): Promise<Result> {
+  //   const { calldata } = await SwapQuoter.quoteCallParameters(
+  //     route,
+  //     CurrencyAmount.fromRawAmount(tokenIn, utils.formatUnits(amountIn, tokenIn.decimals)),
+  //     TradeType.EXACT_INPUT,
+  //     {
+  //       useQuoterV2: true,
+  //     },
+  //   );
 
-    const quoteCallReturnData = await this.provider.call({
-      to: QUOTER_CONTRACT_ADDRESS,
-      data: calldata,
-    });
+  //   const quoteCallReturnData = await this.provider.call({
+  //     to: QUOTER_CONTRACT_ADDRESS,
+  //     data: calldata,
+  //   });
 
-    return utils.defaultAbiCoder.decode(['uint256'], quoteCallReturnData);
-  }
+  //   return utils.defaultAbiCoder.decode(['uint256'], quoteCallReturnData);
+  // }
 }
