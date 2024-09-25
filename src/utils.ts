ac036da0cc790f2a3ce157eec3529ce7ade6d0ca
banri <banri.yanahama@gmail.com>
2024-09-25 16:44:34 +0900

    fix finding input token transfer event #6

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,8 @@
 
 import { DutchInput, DutchOutput } from '@banr1/uniswapx-sdk';
 import { ERC20 } from './types/typechain';
+import { utils } from 'ethers';
+import { Address } from './types/hash';
 
 export function nowTimestamp() {
   return Math.floor(Date.now() / 1000);
@@ -20,4 +22,8 @@ export function getTargetToken(
 
 export async function sleep(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export function topicToAddress(topic: string): Address {
+  return utils.defaultAbiCoder.decode(['address'], topic)[0];
 }
